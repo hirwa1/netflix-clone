1de9f3969876f43742ab3f7c2c8d2b9d088eca4e
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-11 10:39:14 +0200

    ui

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -8,6 +8,12 @@ function App() {
        <h1>Hey This NetFLix Clone</h1>
        <Row title="MetFlix Orginals" fetchUrl={requests.fetchNetflixOriginals} />
        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+       <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
