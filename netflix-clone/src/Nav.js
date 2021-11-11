fcfb34cb83c56be2ceff4a12ade56d08362d9243
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-11 13:06:16 +0200

    yu

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 function Nav() {
     return (
-        <div>
-            
+        <div class="nav">
+            <img 
+             scr
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+            className="nav_logo"
+            alt="Netflix Logo"
+            />
+
+<img 
+             scr
+        src="
+        https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMti6Y58F9U28BKZAOQMWDh9auJ5gfJahe5uKYMjr0kSNaXP4MZYkvDomyRUVKOfiPT5g&usqp=CAU"
+            className="nav_logo"
+            alt="Netflix Logo"
+            />
         </div>
     )
 }
 
 export default Nav
+
