dab13fb7b8f9f47c1d637ccecbe7d311b0257c3c
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-11 10:18:46 +0200

    yu

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -30,6 +30,7 @@ function Row({ title, fetchUrl}) {
             {/* row_posters*/}
             {movies.map(movie => (
                 <img 
+                      key={movie.id}
                       className="row_poster"
                       src={`${base_url}${movie.poster_path}`} alt={movie.name} /> 
             ))}
