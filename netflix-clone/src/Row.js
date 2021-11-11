b89594a4d981d6787541b475e582bdbb82241ee0
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-11 14:09:43 +0200

    ui

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
 import "./Row.css";
+import YouTube from "react-youtube";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
 
 
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTailerUrl] =useState();
 
     // SNipet code 
     useEffect( () =>{
@@ -19,6 +21,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
        }
        fetchData();
     }, [fetchUrl]);
+
+    const opts = {
+        height:"390",
+        width:"100%",
+        playerVars: {
+            autoplay:1,
+        },
+    };
 
     console.log(movies);
     return (
@@ -38,7 +48,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
            </div>
 
 
-            {/* contaniner -> poster */}
+         {trailerUrl &&   <YouTube videoId={trailerUrl} opts={opts} /> }
 
           
         </div>
