1cb38ae8b0f20751632a340ef1ee7cc861401e53
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-11 09:10:30 +0200

    yu

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react;'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <h1>Hey YHis IS hIrwa</h1>
+       <Row title="MetFlix Orginals" />
+       <Row title="Trending Now" />
     </div>
   );
 }
