f991edb686a608097cdbbc780f047062559fc3bf
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-11 10:56:37 +0200

    ty

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -6,10 +6,13 @@ function App() {
   return (
     <div className="App">
        <h1>Hey This NetFLix Clone</h1>
-       <Row title="MetFlix Orginals" fetchUrl={requests.fetchNetflixOriginals} />
+       <Row 
+       
+       title="MetFlix Orginals" 
+       fetchUrl={requests.fetchNetflixOriginals}
+       isLargeRow
+        />
        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
        <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -3,7 +3,7 @@ import axios from './axios';
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl}) {
+function Row({ title, fetchUrl, isLargeRow }) {
 
 
     const [movies, setMovies] = useState([]);
