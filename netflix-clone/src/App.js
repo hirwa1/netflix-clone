6f2d6accfbb9cf0f75030c09e610f8a6e6f708db
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-13 09:25:57 +0200

    ty

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -3,6 +3,7 @@ import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
 import Nav from './Nav';
+import './App.css';
 function App() {
   return (
     <div className="app">
